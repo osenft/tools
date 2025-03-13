b835b8880aaeb5a4d2455289fdf6eb757245501e
Oskar Senft <osk@google.com>
2025-03-13 11:15:23 -0400

    Add monitor-autoscale

---
+++ monitor-autoscale.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+POLL_DELAY=3
+
+OLD_DUAL="dummy"
+
+while true; do
+    DUAL=$(cat /sys/class/drm/card0-DP-1/status)
+
+    if [ "$OLD_DUAL" != "$DUAL" ]; then
+        OLD_DUAL=$DUAL
+        NOW="$(date "+%Y-%m-%d %H:%M:%S")"
+        if [ "$DUAL" == "connected" ]; then
+            echo "$NOW - External monitor connected"
+            gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+        else
+            echo "$NOW - External monitor disconnected"
+            gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
+        fi
+    fi
+
+    sleep $POLL_DELAY
+done
+
---
+++ run-monitor-autoscale.sh
@@ -0,0 +1,1 @@
+screen -d -m -S monitor-autoscale bash -c "$(dirname $0)/monitor-autoscale.sh"
