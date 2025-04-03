d6624e646e1e9a69a9b0c640409050674e8e6037
Oskar Senft <osk@google.com>
2025-04-03 07:41:55 -0400

    Add support for two ports

--- monitor-autoscale.sh
+++ monitor-autoscale.sh
@@ -5,17 +5,17 @@ POLL_DELAY=3
 OLD_DUAL="dummy"
 
 while true; do
-    DUAL=$(cat /sys/class/drm/card0-DP-1/status)
+    DUAL="-$(cat /sys/class/drm/card0-DP-1/status)-$(cat /sys/class/drm/card0-DP-3/status)-"
 
     if [ "$OLD_DUAL" != "$DUAL" ]; then
         OLD_DUAL=$DUAL
         NOW="$(date "+%Y-%m-%d %H:%M:%S")"
-        if [ "$DUAL" == "connected" ]; then
+        if [[ "$DUAL" == *"-connected-"* ]]; then
             echo "$NOW - External monitor connected"
             gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
         else
             echo "$NOW - External monitor disconnected"
-            gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
+            gsettings set org.gnome.desktop.interface text-scaling-factor 1.1
         fi
     fi
 
